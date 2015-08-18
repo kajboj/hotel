6e1ae0d79d419be6ae8eb86aebdae8f1e365e697
Kajetan Bojko <kai@shutl.co.uk>
2015-08-18 23:24:42 +0100

    one more spec

--- spec/x_spec.rb
+++ spec/x_spec.rb
@@ -3,4 +3,13 @@ describe do
   specify do
     expect(naive(3)).to eq([0, 1, 1, 1, 0, 0, 0, 0, 0, 0])
   end
+
+  specify do
+    expect(naive(13)).to eq([1, 6, 2, 2, 1, 1, 1, 1, 1, 1])
+  end
+
+  specify do
+    expect(naive(763)).to eq(
+      [146, 257, 257, 257, 256, 256, 250, 210, 146, 146])
+  end
 end
